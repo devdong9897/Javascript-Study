66e97dfcb583df0f8b65a315fbe690352d3396aa
devdong9897 <dev.dong9897@gmail.com>
2024-12-09 16:48:40 +0900

    feat: 함수

---
+++ section01/chapter11.js
@@ -0,0 +1,24 @@
+// 함수선언
+
+let area1 = getArea(10, 20);
+console.log(area1);
+
+let area2 = getArea(30, 20);
+console.log(area2);
+
+let area3 = getArea(120, 200);
+console.log(area3);
+
+// 호이스팅
+// -> 끌어올리다 라는 뜻
+function getArea(width, height) {
+  function anther() {
+    // 중첩 함수
+    console.log("anther");
+  }
+
+  anther();
+  let area = width * height;
+
+  return area;
+}
